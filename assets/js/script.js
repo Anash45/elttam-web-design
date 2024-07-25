c140b67c8d3b89dc9f4cf8f9dd327f8ce414c57d
Syed Anas Bukhari <anas14529@gmail.com>
2024-07-25 08:46:10 +0500

    Elttam

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    if ($('.article-slider').length > 0) {
+        $('.article-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        nav: false
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        nav: false
+                    }
+                }
+            ]
+        });
+    }
+});
